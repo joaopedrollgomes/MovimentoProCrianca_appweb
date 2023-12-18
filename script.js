14d98706c3824c098819b56bae8340cbdb79966f
Marcela Ellen <marcela.ellen867@gmail.com>
2023-12-18 15:10:23 -0300

    header, menu e form cadastro

---
+++ script.js
@@ -0,0 +1,12 @@
+
+ // biblioteca que coloca o formato do CPF
+ $(document).ready(function () {
+    $('#cpf').inputmask('999.999.999-99');
+});
+$(document).ready(function () {
+    $('#telefone').inputmask('(99) 99999-9999');
+});
+$(document).ready(function () {
+    $('#dataNascimento').inputmask('99/99/9999');
+});
+
